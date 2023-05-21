5851c785ac6a34eeac2185ea2f69f45093fb5fc8
TJenniferS <tanishqsharma20231@gmail.com>
2023-05-21 19:28:54 -0400

    first index head of the array function yield with assertEqual

---
+++ head.js
@@ -0,0 +1,31 @@
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🐱Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+/// Prior Test Code
+//assertEqual("Lighthouse Labs", "Bootcamp"); // Non-identical strings
+//assertEqual("meow", "meow"); // Identical strings
+//assertEqual(10, 10); // Identical numbers
+//assertEqual(32, 9); // Non-identical numbers
+//assertEqual("meow", "Meow"); // Similar strings
+
+
+// Function Implementation
+const head = function(array) {
+  if (array.length === 0) {
+    return undefined;
+  }
+  return array[0];
+};
+
+// Test cases
+assertEqual(head([5, 6, 7]), 5); //yields head 0
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //yields head 0
+assertEqual(head([602]), 602); //yields the only one element as its head
+assertEqual(head(["meow"]), "meow"); //yields the only one element as its head
+assertEqual(head([]), undefined); //yields undefined as it's empty
