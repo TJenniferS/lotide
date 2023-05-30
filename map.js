96b4b28081941d6430654008c2dbef5c2104502a
TJenniferS <tanishqsharma20231@gmail.com>
2023-05-30 19:21:13 -0400

    shorter tests removing unnecessary variables and just passed results of map function calls as arguments to assertArraysEqual and no need to declare separate variables

--- map.js
+++ map.js
@@ -24,18 +24,30 @@ const map = function(array, callback) {
   return array.map(callback);
 };
 
-// Test Case 1: Mapping array elements to their lengths
+// // Test Case 1: Mapping array elements to their lengths
+// const words = ["ground", "control", "to", "major", "tom"];
+// const results1 = map(words, word => word.length);
+// const expected1 = [6, 7, 2, 5, 3];
+// assertArraysEqual(results1, expected1);
+
+// // Test Case 2: Mapping array elements to uppercase
+// const results2 = map(words, word => word.toUpperCase());
+// const expected2 = ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"];
+// assertArraysEqual(results2, expected2);
+
+// // Test Case 3: Mapping array elements to their first character
+// const results3 = map(words, word => word[0]);
+// const expected3 = ["g", "c", "t", "m", "t"];
+// assertArraysEqual(results3, expected3);
+
+// Test Cases
 const words = ["ground", "control", "to", "major", "tom"];
-const results1 = map(words, word => word.length);
-const expected1 = [6, 7, 2, 5, 3];
-assertArraysEqual(results1, expected1);
+
+// Test Case 1: Mapping array elements to their lengths
+assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3]);
 
 // Test Case 2: Mapping array elements to uppercase
-const results2 = map(words, word => word.toUpperCase());
-const expected2 = ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"];
-assertArraysEqual(results2, expected2);
+assertArraysEqual(map(words, word => word.toUpperCase()), ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"]);
 
 // Test Case 3: Mapping array elements to their first character
-const results3 = map(words, word => word[0]);
-const expected3 = ["g", "c", "t", "m", "t"];
-assertArraysEqual(results3, expected3);
+assertArraysEqual(map(words, word => word[0]), ["g", "c", "t", "m", "t"]);
