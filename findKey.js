14d2c2b633f5786d753d629f6415118d58e65004
TJenniferS <tanishqsharma20231@gmail.com>
2023-05-30 20:37:45 -0400

    finished findKey with test function that has object restaurants and their ratings then uses assertequal to use findkey function to print noma or undefined

--- findKey.js
+++ findKey.js
@@ -19,3 +19,15 @@ const findKey = (object, callback) => {
   return undefined;
 };
 
+// Test cases
+const restaurantRatings = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+};
+
+assertEqual(findKey(restaurantRatings, x => x.stars === 2), "noma");
+assertEqual(findKey(restaurantRatings, x => x.stars === 4), undefined);
