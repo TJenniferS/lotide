ec81c9096c83c6a5288ac190917ba478259dbda5
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-12 01:26:02 -0400

    more test codes and deleted old asserts

--- test/headTest.js
+++ test/headTest.js
@@ -1,22 +1,24 @@
-// const assertEqual = require('../assertEqual');
-// const head = require('../head');
-
-// assertEqual(head([5, 6, 7]), 5); //yields head 0
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //yields head 0
-// assertEqual(head([602]), 602); //yields the only one element as its head
-// assertEqual(head(["meow"]), "meow"); //yields the only one element as its head
-// assertEqual(head([]), undefined); //yields undefined as it's empty
-
 const assert = require('chai').assert;
 const head   = require('../head');
 
 describe("#head", () => {
-  it("returns 1 for [1, 2, 3]", () => {
+  it("returns the first element 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
 
-  it("returns '5' for ['5']", () => {
+  it("returns the first element '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5');
   });
 
+  it("returns undefined for an empty array", () => {
+    assert.isUndefined(head([]));
+  });
+
+  it("returns the first element of an array with multiple elements", () => {
+    assert.strictEqual(head([10, 20, 30, 40]), 10);
+  });
+
+  it("returns the first element of an array with different data types", () => {
+    assert.strictEqual(head(['mango', 100, true, { name: 'Cat' }]), 'mango');
+  });
 });
