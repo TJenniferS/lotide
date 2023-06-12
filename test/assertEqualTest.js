c3538672a23e270a9982c150746bf2fd498d4c94
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-12 01:50:09 -0400

    middle and tail tests changed to mocha

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -5,3 +5,4 @@ assertEqual("meow", "meow"); // Identical strings
 assertEqual(10, 10); // Identical numbers
 assertEqual(32, 9); // Non-identical numbers
 assertEqual("meow", "Meow"); // Similar strings
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,40 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); //we only want 2 elements
+// assertEqual(result[0], "Lighthouse"); //first element will be "Lighthouse"
+// assertEqual(result[1], "Labs"); //2nd element is "Labs"
+
+// //Test case: Check original array
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); //not capturing return value since it's not what we're checking
+// assertEqual(words.length, 3); //original array should still have 3 elements
+
+const assert = require('chai').assert;
+// const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); //we only want 2 elements
-assertEqual(result[0], "Lighthouse"); //first element will be "Lighthouse"
-assertEqual(result[1], "Labs"); //2nd element is "Labs"
+describe("#tail", () => {
+  it("should return an array of length 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+  });
 
-//Test case: Check original array
+  it("should return 'Lighthouse' as the first element for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[0], "Lighthouse");
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); //not capturing return value since it's not what we're checking
-assertEqual(words.length, 3); //original array should still have 3 elements
+  it("should return 'Labs' as the second element for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[1], "Labs");
+  });
+
+  it("should not modify the original array ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+});
