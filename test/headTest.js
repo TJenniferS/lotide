a891362f6de655a587e5f5e509159bba8e58f42e
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-12 01:17:53 -0400

    added tests from assignment

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,22 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+// const assertEqual = require('../assertEqual');
+// const head = require('../head');
 
-assertEqual(head([5, 6, 7]), 5); //yields head 0
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //yields head 0
-assertEqual(head([602]), 602); //yields the only one element as its head
-assertEqual(head(["meow"]), "meow"); //yields the only one element as its head
-assertEqual(head([]), undefined); //yields undefined as it's empty
+// assertEqual(head([5, 6, 7]), 5); //yields head 0
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //yields head 0
+// assertEqual(head([602]), 602); //yields the only one element as its head
+// assertEqual(head(["meow"]), "meow"); //yields the only one element as its head
+// assertEqual(head([]), undefined); //yields undefined as it's empty
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+});
