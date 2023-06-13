3859f717ac16a05c24308de6886a8a74b60aef49
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-13 15:22:50 -0400

    remove assertEqual comment out code, remove test cases code comment out, add indication of testing with assertEqual module

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// Function Implementation assert equal
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✔️Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
 // Function Implementation eqArrays
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -22,9 +13,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-// // Test Cases
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-module.exports = eqArrays;
+module.exports = eqArrays; // tests with assertEqual
