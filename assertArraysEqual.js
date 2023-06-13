765fe2a052d40855def91f0b588edba0e7760667
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-13 14:23:59 -0400

    removed commented out function eqarrays and added comment about the line that requires it, deleted test cases because test file is receiving exported assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,4 @@
-const eqArrays = require('./eqArrays');
-// Function Implementation eqArrays
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// };
+const eqArrays = require('./eqArrays'); // Require eqArrays module from this directory
 
 // Function Implementation assertArraysEqual
 const assertArraysEqual = function(arr1, arr2) {
@@ -20,9 +6,5 @@ const assertArraysEqual = function(arr1, arr2) {
   console.log(`${assertionMessage}: [${arr1}] === [${arr2}]`);
 };
 
-// // Test Cases
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 3], [3, 2, 1]);
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
 module.exports = assertArraysEqual;
+
