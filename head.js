3ec43b201cc045e4d7ff7e5ba05ecd9dc08ff826
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-13 15:27:17 -0400

    remove assertEqual function and prior incorrect assertEqual test code and require assertEqual because it is  in test and remove comment out test cases from head code

--- head.js
+++ head.js
@@ -1,22 +1,3 @@
-// const assertEqual = require('./assertEqual');
-
-//base assertEqual Function Implementation
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✔️Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-/// Prior Test Code
-//assertEqual("Lighthouse Labs", "Bootcamp"); // Non-identical strings
-//assertEqual("meow", "meow"); // Identical strings
-//assertEqual(10, 10); // Identical numbers
-//assertEqual(32, 9); // Non-identical numbers
-//assertEqual("meow", "Meow"); // Similar strings
-
-
 // Define head Function Implementation
 const head = function(array) {
   //start from first element
@@ -26,10 +7,4 @@ const head = function(array) {
   return array[0];
 };
 
-// Test cases for head function
-// assertEqual(head([5, 6, 7]), 5); //yields head 0
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //yields head 0
-// assertEqual(head([602]), 602); //yields the only one element as its head
-// assertEqual(head(["meow"]), "meow"); //yields the only one element as its head
-// assertEqual(head([]), undefined); //yields undefined as it's empty
 module.exports = head;
