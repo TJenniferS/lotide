af1eeb7e9acd7462ebacf48e59a7b205f10dd6ea
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-13 15:37:35 -0400

    remove comment out eqArrays, assertArraysEqual, assertEqual functions. remove test assert cases and for middle because middle exports to chai test file

--- middle.js
+++ middle.js
@@ -1,39 +1,4 @@
-// // Function Implementation eqArrays
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// };
-
-// // Function Implementation assertArraysEqual
-// const assertArraysEqual = function(arr1, arr2) {
-//   const assertionMessage = eqArrays(arr1, arr2) ? "✔️Assertion Passed" : "❌Assertion Failed";
-//   console.log(`${assertionMessage}: [${arr1}] === [${arr2}]`);
-// };
-
-
-
-
-// Function Implementation
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✔️Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-
-
-//actual function
+// middle function
 const middle = function(array) {
   const middleArray = [];
   const length = array.length;
@@ -51,26 +16,5 @@ const middle = function(array) {
 
   return middleArray;
 };
-
-
-
-// // Test Cases assert
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 3], [3, 2, 1]);
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-// // Test Cases eqArrays
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
-// //test code middle
-// assertArraysEqual(middle([1]), []); // Empty array
-// assertArraysEqual(middle([1, 2]), []); // Empty array
-// assertArraysEqual(middle([1, 2, 3]), [2]); // Single middle element
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Single middle element
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Two middle elements
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Two middle elements
 
 module.exports = middle;
