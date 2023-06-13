dc915d0a4274919f435bd7a59f5b993d51f838f0
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-13 15:20:49 -0400

    remove comment out code and unnecessary comments, add comments to specify code functionality

--- countOnly.js
+++ countOnly.js
@@ -1,7 +1,4 @@
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
-
-
+// use assertEqual to test
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
@@ -10,30 +7,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countOnly = function(allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) { // itemsToCount: an object specifying what to count
+// allItems: an array of strings that we need to look through
   const results = {};
-  //When accessing property Jason on an empty object, JS will not throw an error. It will instead give us back undefined since the property does not exist on it.The second check (for Karima) expects undefined and will therefore actually pass! The first and last ones (for Fang and Jason) will fail complaining because their expected counts will be compared with undefined.
-
-  // const countOnly = function(allItems, itemsToCount) {
-  //   const results = {};
-  
-  //   for (const item of allItems) {
-  //     console.log(item);
-  //   }
-  
-  //   return results;
-  // }
-
-  //output
-  // Karl
-  // Salima
-  // Agouhanna
-  // Fang
-  // Kavith
-  // Jason
-  // Salima
-  // Fang
-  // Joe
 
   for (const item of allItems) {
     if (itemsToCount[item]) { //only increment our count in results if item found in itemsToCount object
@@ -63,7 +39,7 @@ const firstNames = [
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-//can only compare primitive values
+// testing with primitive values
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
