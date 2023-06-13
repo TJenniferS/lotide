9291426a552993ebd8d99b896e5b05bfb5d33fd4
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-13 15:40:38 -0400

    remove comment out code assertEqual, remove comment out code all tests and terminal expected output, add comment for export to test file

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-// base assertEqual Function Implementation
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✔️Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌will always fail! Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
 //Define tail Function Implementation
 const tail = function(array) {
   // Check if the array has at least one element
@@ -18,29 +9,4 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-// Test Cases for tail function
-//..assertEqual(tail([5, 6, 7]), [6, 7]);
-//..assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-
-
-
-///test (this works but feels clunky)
-//let a = tail([5, 6, 7]);
-//let b = [6, 7];
-//assertEqual(a[0], b[0]);
-
-// Check the returned array elements from new function defined as result of tail
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.length, 2); //we only want 2 elements
-// assertEqual(result[0], "Lighthouse"); //first element will be "Lighthouse"
-// assertEqual(result[1], "Labs"); //2nd element is "Labs"
-
-// //Test case: Check original array
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); //not capturing return value since it's not what we're checking
-// assertEqual(words.length, 3); //original array should still have 3 elements
-
-//Terminal returns
-// ✔️Assertion Passed: 3 === 3
 module.exports = tail;
